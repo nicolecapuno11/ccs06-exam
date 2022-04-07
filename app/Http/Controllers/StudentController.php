4851ec038f9d2c6bc8df3181e338f8f764dc0f1b
nicolecapuno_11 <colecapuno@gmail.com>
2022-04-07 10:34:24 +0800

    initial commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -34,6 +34,19 @@ class StudentController extends Controller
         return round($average, 2);
     }
 
+    protected function getRemarks($grade_remarks)
+    {
+        if($grade_remarks >= 75)
+        {
+            $remarks = "PASSED";
+        }
+        else 
+        {
+            $remarks = "FAILED"; 
+        }
+        return $remarks;
+    }
+
     public function computeGrades(Request $request)
     {
         $student_1 = $request->student_1;
@@ -47,6 +60,12 @@ class StudentController extends Controller
         $s3_average = $this->computeAverageScore($request->s3_midterm, $request->s3_final);
         $s4_average = $this->computeAverageScore($request->s4_midterm, $request->s4_final);
         $s5_average = $this->computeAverageScore($request->s5_midterm, $request->s5_final);
+
+        $s1_remarks = $this->getRemarks($s1_average);
+        $s2_remarks = $this->getRemarks($s2_average);
+        $s3_remarks = $this->getRemarks($s3_average);
+        $s4_remarks = $this->getRemarks($s4_average);
+        $s5_remarks = $this->getRemarks($s5_average);
 
         return view('compute-grades', [
             'student_1' => $student_1,
@@ -63,18 +82,22 @@ class StudentController extends Controller
             's2_midterm' => $request->s2_midterm,
             's2_final' => $request->s2_final,
             's2_average' => $s2_average,
+            's2_remarks' => $s2_remarks,
             // Student 3 Grades
             's3_midterm' => $request->s3_midterm,
             's3_final' => $request->s3_final,
             's3_average' => $s3_average,
+            's3_remarks' => $s3_remarks,
             // Student 4 Grades
             's4_midterm' => $request->s4_midterm,
             's4_final' => $request->s4_final,
             's4_average' => $s4_average,
+            's4_remarks' => $s4_remarks,
             // Student 5 Grades
             's5_midterm' => $request->s5_midterm,
             's5_final' => $request->s5_final,
             's5_average' => $s5_average,
+            's5_remarks' => $s5_remarks
         ]);
     }
 }
